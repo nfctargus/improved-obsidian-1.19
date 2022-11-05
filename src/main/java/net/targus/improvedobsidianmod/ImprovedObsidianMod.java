e676d385142a9454d7adaca028345bc5b1f651b2
nfctargus <kieran.lawrence@hotmail.com>
2022-11-05 15:15:59 +0800

    Initial upload

---
+++ src/main/java/net/targus/improvedobsidianmod/ImprovedObsidianMod.java
@@ -0,0 +1,15 @@
+package net.targus.improvedobsidianmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImprovedObsidianMod implements ModInitializer {
+	public static final String MOD_ID = "improvedobsidianmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/targus/improvedobsidianmod/ImprovedObsidianModClient.java
@@ -0,0 +1,10 @@
+package net.targus.improvedobsidianmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImprovedObsidianModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
